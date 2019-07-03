7b9836930525f66c8eb00b3dec917fcc63d84fbe
Victor <victor@turo.com>
2019-07-03 15:17:46 -0700

    chore(hyper): add statusline and search plugins

--- hyper.js
+++ hyper.js
@@ -5,32 +5,8 @@ module.exports = {
       'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
     cursorColor: "rgba(248,28,229,0.8)",
     cursorShape: "BLOCK",
-    cursorBlink: false,
-    foregroundColor: "#fff",
-    backgroundColor: "#000",
     borderColor: "#333",
-    css: "",
-    termCSS: "",
-    showHamburgerMenu: "",
     padding: "12px 14px",
-    colors: {
-      black: "#000000",
-      red: "#ff0000",
-      green: "#33ff00",
-      yellow: "#ffff00",
-      blue: "#0066ff",
-      magenta: "#cc00ff",
-      cyan: "#00ffff",
-      white: "#d0d0d0",
-      lightBlack: "#808080",
-      lightRed: "#ff0000",
-      lightGreen: "#33ff00",
-      lightYellow: "#ffff00",
-      lightBlue: "#0066ff",
-      lightMagenta: "#cc00ff",
-      lightCyan: "#00ffff",
-      lightWhite: "#ffffff"
-    },
     shell: "/bin/zsh",
     shellArgs: ["--login"],
     env: {},
@@ -45,7 +21,8 @@ module.exports = {
     "hypercwd",
     "hyperlinks",
     "hyper-nord",
-    "hyper-statusline"
+    "hyper-statusline",
+    "hyper-search"
   ],
   localPlugins: []
 };
