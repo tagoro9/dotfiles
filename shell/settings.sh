4377e7b20b941495508df125c7136832662971da
Victor <victor@turo.com>
2020-01-31 14:56:36 -0800

    chore(settings): add default NODE_OPTIONS and go config

--- shell/settings.sh
+++ shell/settings.sh
@@ -4,13 +4,19 @@ export EDITOR=vim
 # Ant configuration
 export ANT_OPTS='-Xms768m -Xmx1024m'
 
+# Increase node memory by default
+export NODE_OPTIONS='--max-old-space-size=2048'
+
 # Color configuration
 export LS_OPTIONS='--color=auto'
 export CLICOLOR='Yes'
 export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 
+# GO configuration
+GOPATH=$HOME/go
+
 # Path configuration
-export PATH="$HOME/.yarn/bin:$HOME/bin:$HOME/.jenv/bin:$PATH"
+export PATH="$HOME/.yarn/bin:$HOME/bin:$HOME/.jenv/bin:/usr/local/go/bin:$GOPATH/bin:$PATH"
 
 # Confiure jenv
 eval "$(jenv init -)"
