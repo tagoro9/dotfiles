3ece5782866b591894427a15b3f094e801fc8be4
Victor <victor@turo.com>
2024-09-18 03:21:56 +0000

    feat(shell): set better colors for exa

--- config/shells/common/settings.sh
+++ config/shells/common/settings.sh
@@ -33,3 +33,22 @@ if [ -d "$PROFILE_DIR" ]; then
     done
 fi
 
+# Dracula theme for exa
+# https://draculatheme.com/exa
+export EXA_COLORS="\
+uu=36:\
+gu=37:\
+sn=32:\
+sb=32:\
+da=34:\
+ur=34:\
+uw=35:\
+ux=36:\
+ue=36:\
+gr=34:\
+gw=35:\
+gx=36:\
+tr=34:\
+tw=35:\
+tx=36:"
+
