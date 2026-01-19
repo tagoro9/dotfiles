6cbfb5e30f488afe3e3e0811755c7f16818260f2
Victor Mora <1235917+tagoro9@users.noreply.github.com>
2026-01-18 18:18:45 -0800

    fix(fzf): ensure options are persisted and work inside zellij

--- config/private_dot_config/shell/completions.zsh
+++ config/private_dot_config/shell/completions.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 
 fpath+="$HOME/.config/shell/completions"
+
+# For some reason \n don't work well for this variable inside zellij
+# Tiny update to https://github.com/unixorn/fzf-zsh-plugin/blob/909f0b8879481eab93741fa284a7d1d13cf6f79e/fzf-zsh-plugin.plugin.zsh#L149C1-L149C68
+export FZF_DEFAULT_OPTS=$(printf '%s  ' "${FZF_DEFAULT_OPTS[@]//$'\n'/ }")
